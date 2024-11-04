2c82c49257770c22236723585a849154038b0425
carpx05 <aa1465@srmist.edu.in>
2024-11-04 10:07:02 +0530

    test: presciption without regex.

--- flask-backend/src/__init__.py
+++ flask-backend/src/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_cors import CORS 
-
+from src.service.heartbeat import HeartbeatService
 
 
 def create_app():
@@ -9,7 +9,14 @@ def create_app():
     from src.routes.predict_route import predict_bp
     from src.routes.prescription_route import prescription_bp
     from src.routes.chat_route import chat_bp
+    from src.routes.heartbeat_route import heartbeat_bp
+    app.register_blueprint(heartbeat_bp)
     app.register_blueprint(predict_bp)
     app.register_blueprint(prescription_bp)
     app.register_blueprint(chat_bp)
+
+    heartbeat = HeartbeatService(app)
+    with app.app_context():
+        heartbeat.start()
+
     return app
